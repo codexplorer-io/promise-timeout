9514ae075a0816a9d7e59550900b5239f212a8bd
Marko Matic <mail.marko.matic@gmail.com>
2023-02-03 09:46:07 +1100

    - Clear promise timeout

--- src/index.js
+++ src/index.js
@@ -7,10 +7,28 @@ export const withPromiseTimeout = (
         message = 'Promise has timed out.'
     } = options;
 
+    let promiseTimeout;
+    const clearPromiseTimeout = () => {
+        promiseTimeout !== undefined && clearTimeout(promiseTimeout);
+        promiseTimeout = undefined;
+    };
+
     return Promise.race([
-        promise,
+        (async () => {
+            try {
+                const result = await promise;
+                clearPromiseTimeout();
+                return result;
+            } catch (error) {
+                clearPromiseTimeout();
+                throw error;
+            }
+        })(),
         new Promise((_, reject) => {
-            setTimeout(() => reject(new Error(message)), timeoutInMillis);
+            promiseTimeout = setTimeout(() => {
+                promiseTimeout = undefined;
+                reject(new Error(message));
+            }, timeoutInMillis);
         })
     ]);
 };
